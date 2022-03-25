bea567e0f03569f3ad5befe2cd2411e37f420f02
Søren Fallesen <sfa@akait.dk>
2022-03-25 16:10:11 +0100

    Added support for gzipfilter in undertow

--- plugins/modules/jcliff.py
+++ plugins/modules/jcliff.py
@@ -1592,7 +1592,41 @@ def main():
                                             credential=dict(type='str', required=False),
                                             use_resource_role_mappings=dict(type='bool', required=False),
                                             disable_trust_manager=dict(type='bool', required=False),
-                                        )))))),
+                                        )))),
+                            undertow=dict(
+                              type="list", required=False, elements='dict', options=dict(
+                                configuration=dict(
+                                  type='list', required=False, elements='dict', options=dict(
+                                    filter=dict(
+                                      type="list", required=False, elements='dict', options=dict(
+                                        gzip=dict(type='str', required=False)
+                                      )
+                                    )
+                                  )
+                                ),
+                                server=dict(
+                                  type="list", required=False, elements='dict', options=dict(
+                                    name=dict(type='str', required=True),
+                                    host=dict(
+                                      type="list", required=False, elements='dict', options=dict(
+                                        name=dict(type='str', required=True),
+                                        filter_ref=dict(
+                                          type="list", required=False, elements='dict', options=dict(
+                                            gzipfilter=dict(
+                                              type="list", required=False, elements='dict', options=dict(
+                                                predicate=dict(type='str',required=True)
+                                              )
+                                            )
+                                          )
+                                        )
+                                      )
+                                    )
+                                  )
+                                )
+                              )
+                            )            
+                                        
+                                        )),
         state=dict(default="present", choices=[
                    'present', 'absent'], type='str')
     )
